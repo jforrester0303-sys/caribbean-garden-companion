0bc6a28cf3ca0cc2033a9dc12ffdc61d277bf5f4
jforrester0303-sys <jforrester0303@gmail.com>
2026-07-24 14:03:20 -0500

    Create services index file for easy imports

---
+++ mobile_app/lib/services/index.dart
@@ -0,0 +1,12 @@
+/// Services Index - Central import file for all services
+export 'auth_service.dart';
+export 'garden_service.dart';
+export 'container_service.dart';
+export 'plant_service.dart';
+export 'water_log_service.dart';
+export 'rain_log_service.dart';
+export 'harvest_service.dart';
+export 'journal_service.dart';
+export 'task_service.dart';
+export 'notification_service.dart';
+export 'storage_service.dart';
